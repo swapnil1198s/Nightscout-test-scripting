ecf96c9cdb627c0f4d10af6a53d8a4bffd721f6c
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-10-03 12:42:57 -0400

    new template and main script boilerplate

---
+++ TestAutomation/scripts/runAllTests.py
@@ -0,0 +1,13 @@
+
+
+# delete old html, generate new empty one
+
+# For each File in Test Cases
+#     delete all executables
+#     generate executable from file
+        # import from script
+#     subprocess.execute('NPM TEST')
+#     pipe output to resutls.hmtl
+        # import from script
+
+# open 'finished' html file
