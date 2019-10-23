b84390ac088de99117b5a726ef8db1edbb087f01
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-10-23 14:12:16 -0400

    revert foolish change

--- TestAutomation/temp/test-1.js
+++ TestAutomation/temp/test-1.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 describe('units.js mmolToMgdl', function() {
     context('input 1; expected output 18', function(){
         it('testing if converts correctly', function(){
-            expect(unit.mmolToMgdl('1')).to.equal(18)
+            expect(unit.mmolToMgdl(1)).to.equal(18)
         })
     })
 })
