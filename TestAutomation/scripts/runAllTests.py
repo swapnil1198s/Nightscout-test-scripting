0fc92b95cdd1b73c9f7c1d80dd3c2140473e34e0
swapnil srivastava <srivastavas@g.cofc.edu>
2019-10-28 14:22:59 -0400

    made the html representation of report

--- TestAutomation/scripts/runAllTests.py
+++ TestAutomation/scripts/runAllTests.py
@@ -27,11 +27,21 @@ with open(filename, "w") as htmlfile:
     htmlfile.write('''<!DOCTYPE html>\n
     <html lang="en-US" style="height: 100%;">\n
     <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>My List HTML Output</title>\n''')
-
-    for infilepath in os.listdir(casepath):
+    <title>My List HTML Output</title>\n
+    <table style : "width = 100%"  > 
+        <tr> 
+                <th> Test ID </th>
+                <th> Requirement </th>
+                <th> component </th>
+                <th> method </th>
+                <th> input </th>
+                <th> Expected Output </th>
+                <th> Output </th>
+        </tr>''')
+    htmlfile.write('<tr>')
+    for infilepath in sorted(os.listdir(casepath)):
         print('generate test: ', infilepath, outpath)
-
+        
         # exeGen(os.path.join(casepath, infilepath), outfilepath, exectemplatepath)
         # test(os.path.join(casepath, infilepath),outfilepath)
         minigen(os.path.join(casepath, infilepath),outfilepath)
@@ -46,16 +56,23 @@ with open(filename, "w") as htmlfile:
         lines = casefile.readlines()
         casefile.close()
         expectval = lines[-1].split(':')[-1]
-
-        infilepath += ' | '
-        infilepath += foo.oracle(expectval, resval)
-        infilepath += ' | Expected: '+expectval+", Recieved: "+resval+" | "
-        rline = reportline(infilepath,
-        lines)
-
-        htmlfile.write(rline)
+        
+        for input in lines:
+            elementsList = input.split(':')
+            element = elementsList[1]
+            htmlfile.write('<td>' + element + '</td>')
+        
+        #infilepath += ' | '
+        #infilepath += foo.oracle(expectval, resval)
+        #infilepath += ' | Expected: '+expectval+", Recieved: "+resval+" | "
+        #rline = reportline(infilepath,
+        #lines)
+        htmlfile.write('<td>' + resval + '</td>')
+        htmlfile.write('</tr>')
+        #htmlfile.write(rline)
+    
     print("done")
-    htmlfile.write('''</head>''')
+    htmlfile.write('''</head></table>''')
 
 try: subprocess.call(['xdg-open', filename])
 except:
