c65c55df5f80d6dffdbfbfc2910b73bac201c03f
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-11-19 10:46:17 -0500

    fixed breaking units

--- TestAutomation/project/src/units.js
+++ TestAutomation/project/src/units.js
@@ -2,7 +2,7 @@
 
 function mgdlToMMOL(mgdl) {
   //return (Math.round((mgdl / 18) * 10).toFixed(1)/10); #Original Code
-  return ((mgdl / 18) * 10).toFixed(1)/10); //Fault Injection #4
+  return ((mgdl / 18) * 10).toFixed(1)/10; //Fault Injection #4
 }
 
 function mmolToMgdl(mgdl) {
